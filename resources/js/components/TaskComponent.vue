88ef3c034fcf143309b9f84e6f5a91755bad2f7c
Aurelien <aurelien.leclerc7@gmail.com>
2020-10-05 14:46:44 +0200

    adding form for tasks

---
+++ resources/js/components/TaskComponent.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <add-task @task-added="refresh"></add-task>
+        <ul class="list-group">
+            <li class="list-group-item" v-for="task in tasks.data" :key="task.id">
+                <a href="#">{{ task.name }}</a>
+            </li>
+        </ul>
+        <pagination :data="tasks" @pagination-change-page="getResults" class="mt-5"></pagination>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        data(){
+            return{
+                tasks: {}
+            }
+        },
+
+        created(){
+            axios.get('http://127.0.0.1:8000/tasksList')
+                .then(response => this.tasks = response.data)
+                .catch(error => console.log(error));
+        },
+
+        methods: {
+            // Our method to GET results from a Laravel endpoint
+            getResults(page = 1) {
+                axios.get('http://127.0.0.1:8000/tasksList?page=' + page)
+                    .then(response => {
+                        this.tasks = response.data;
+                    });
+            },
+
+            refresh(tasks){
+                this.tasks = tasks.data
+            }
+        },
+
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
